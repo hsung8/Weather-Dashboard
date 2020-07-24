2c8e7bdaf6cfc689117d2d846576905345c431c1
macpro <haeseungsung@MacBook-Pro.local>
2020-07-23 23:45:04 -0400

    add uv

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Weather-Dashboard
 
-This weather dashboard will show you current weather and 5-days forcast for the city you search.
+This weather dashboard will show you current weather and 5-days forcast for the city you search. In details, it shows temperature(F), relative humidity(%), wind speed, and UV index. You can serach cities as much as you want, and click on the cities that you already searched on the history (left bar) to call the previous search result.
 
 The below image is an example of the page.
 
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var citySearch = document.getElementById("citySearch")
 var searchArray = []
 var today = moment().format("M" + "/" + "D" + "/" + "YYYY")
-var APIKey = "c579d0da1a725ebe94f2273b287a623c";
+var api = "c579d0da1a725ebe94f2273b287a623c";
 
 function searchWeather(city) {
 
@@ -9,27 +9,40 @@ function searchWeather(city) {
         $("#forecast" + i).empty();
     }
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         var weatherImg = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png';
-        $("#current-city").html(response.name + " (" + today + ") " + "<img src=" + weatherImg + "> </img>");
+        $("#current-city").html(response.name + " (" + today + ") " + "<img src=" + weatherImg + "> </img>")
 
-        var temp = parseFloat(((response.main.temp) - 273.15) * 1.8 + 32).toFixed(1);
-        $("#temp").html("Current Temperature: " + temp + "<span>&deg</span> F");
+        var temp = parseFloat(((response.main.temp) - 273.15) * 1.8 + 32).toFixed(1)
+        $("#temp").html("Current Temperature: " + temp + "<span>&deg</span> F")
 
-        var humidity = response.main.humidity;
-        $("#humidity").html("Current Humidity: " + humidity + "%");
+        var humidity = response.main.humidity
+        $("#humidity").html("Current Humidity: " + humidity + "%")
 
         var windSpeed = parseFloat(response.wind.speed).toFixed(1);
-        $("#wind").html("Current Wind Speed: " + windSpeed + " MPH");
+        $("#wind").html("Current Wind Speed: " + windSpeed + " MPH")
+
+        //UV
+        var lon = response.coord.lon;
+        var lat = response.coord.lat
+
+        var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?cnt=5&lat=" + lat + "&lon=" + lon + "&APPID=" + api
+
+        $.ajax({
+            url: uvUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.value)
+            $("#uv").html("UV Index: ").append(response.value);
+        })
 
         //forecast
-        queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-        console.log(response)
+        queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + api
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -44,6 +57,7 @@ function searchWeather(city) {
                 $("#forecast" + i).removeClass("d-none");
 
                 weatherImg = 'https://openweathermap.org/img/wn/' + response.list[i].weather[0].icon + '@2x.png'
+                
                 $("<img>").attr("src", weatherImg).appendTo("#forecast" + i);
 
                 $("<p>").html("Temp: " + parseFloat(((response.list[i].main.temp) - 273.15) * 1.8 + 32).toFixed(2) + "<span>&deg</span> F").appendTo("#forecast" + i);
@@ -89,6 +103,7 @@ $("#searchBtn").on("click", function () {
         $("#temp").empty()
         $("#humidity").empty()
         $("#wind").empty()
+        $("#uv").empty()
         $("#forecast-card").empty()
     }
 });
